3cb447faca0fc504b967b6680b3c49de219960a6
Tushar Ranjan <thrrnjn1103@gmail.com>
2025-02-08 20:05:48 +0530

    Add initial React and Vite setup with assets, routing, and configuration files

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<Home/>} />
+        <Route />
+        <Route />
+      </Routes>
+     
+    </>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+         <h1 className="text-4xl bg-slate-500">HELLO FROM STUDY NOTION</h1>
+    </div>
+  )
+}
+
+export default Home
